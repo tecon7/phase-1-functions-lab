a22053df8380e8631c4d4c27a2bc60efa0b9c490
Teddy Economou <teddyecon@gmail.com>
2023-01-10 12:25:32 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(42 - someValue)
+}
+
+function distanceFromHqInFeet(someValue) {
+   return Math.abs(distanceFromHqInBlocks(someValue) * 264)
+  
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs(start - destination) * 264
+}
+
+// expect(distanceFromHqInBlocks(43)).to.equal(1)
+function calculatesFarePrice(start, destination) {
+   let totalDistance = distanceTravelledInFeet(start, destination)
+    if (totalDistance <= 400){
+        return 0;
+    } else if (totalDistance > 400 && totalDistance <= 2000){
+        let totalPrice = (totalDistance - 400)*(.02)
+        return totalPrice;
+    } else if (totalDistance > 2000 && totalDistance < 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+
+
+}
+    
